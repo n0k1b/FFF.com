d678b25aca225fe9b0f19aca6ecd2f8df22b5233
MirReaz <reaz.ctg365@live.com>
2019-03-10 00:04:12 +0600

    "See All" of category udated

--- app/src/main/java/com/simple/reaz/fffcom/SeeAll/SeeAll.java
+++ app/src/main/java/com/simple/reaz/fffcom/SeeAll/SeeAll.java
@@ -1,10 +1,12 @@
-package com.simple.reaz.fffcom;
+package com.simple.reaz.fffcom.SeeAll;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.simple.reaz.fffcom.Home.HomeAdapter;
 import com.simple.reaz.fffcom.Home.ModelCategories;
+import com.simple.reaz.fffcom.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +14,19 @@ import java.util.List;
 public class SeeAll extends AppCompatActivity {
 
     List<ModelCategories> cat_list= new ArrayList<>();
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_all);
 
+        recyclerView=findViewById(R.id.seeAllRecycleView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(SeeAll.this, LinearLayoutManager.VERTICAL, false));
 
         cat_list = (List<ModelCategories>) getIntent().getSerializableExtra("category");
-
-        if(cat_list!=null)
-        Toast.makeText(this, ""+cat_list.get(0).getCat_image()+"\n"+
-                cat_list.get(1).getCat_image(), Toast.LENGTH_SHORT).show();
+        if(cat_list!=null){
+            recyclerView.setAdapter(new SeeAllAdapter(SeeAll.this, cat_list));
+        }
 
     }
 
